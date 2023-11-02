d3e13850c9c9ab1ad26c40fef7a541aedbc35fa7
Jamie Branch <schwung988@yahoo.com>
2023-11-01 22:25:21 -0400

    add screenshot

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './components/NavBar/Navbar.js';
 import Intro from './components/NavBar/Intro/Intro.js';
 import About from './components/About/about.js';
+import Footer from './components/Footer/footer.js';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar/> 
       <Intro/>
       <About />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Footer/footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+        Copyright©2023 Jamie Branch. All rights reserved.
+        </footer>
+    );
+}
+
+export default Footer;
